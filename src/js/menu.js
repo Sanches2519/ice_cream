5305e615df198ab55da036bf13592bee9f7d0b1c
Sanches2519 <dolgoruk.olexsandr@gmail.com>
2023-07-16 18:25:56 +0300

    blue

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,23 @@
-export const menu =
-(() => {
+export const menu = (() => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
-    body: document.querySelector("body"),
+    body: document.querySelector('body'),
   };
+  const link = document.querySelectorAll('.nav__link');
+  link.forEach(a => a.addEventListener('click', removeLink));
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
+  
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
-    refs.body.classList.toggle("no-scroll");
+    refs.body.classList.toggle('no-scroll');
+  }
+  function removeLink() {
+    refs.menu.classList.remove('is-open');
+    refs.body.classList.remove('no-scroll');
   }
 })();
-
-
-
